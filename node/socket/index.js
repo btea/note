0c92ea5f0d20b506a0dc1bd61068a576f2010a72
曾佑森 <2356281422@qq.com>
2019-03-17 21:36:46 +0800

    ws

---
+++ node/socket/index.js
@@ -0,0 +1,22 @@
+const http = require('http');
+const fs = require('fs');
+const webSocketServer = require('ws').Server;
+
+http.createServer(function(request, response) {
+    fs.readFile('./index.html','utf-8', function(err, data){
+        if(err){return;}
+        response.write(data);
+        response.end();
+    })
+}).listen(3333);
+
+let wsServer = new webSocketServer({port: 2333});
+wsServer.on('connection', function(socket){
+    // socket.onmessage = function(event){
+
+    // }
+    socket.on('message', function(message){
+        console.log('接受到的消息:', message);
+        socket.send('来自服务端的消息:' + message);
+    })
+})
