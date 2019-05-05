d68b48d3e71660b80e6430f25e32814016422565
btea <2356281422@qq.com>
2019-05-05 18:04:55 +0800

    curve

--- canvas/chart.js
+++ canvas/chart.js
@@ -52,7 +52,9 @@ class Chart{
     drawLine(points, smooth = false){
         this.ctx.lineWidth = 2;
         if(smooth){
-            this.smoothCurve(points);
+            // this.smoothCurve(points);
+            this.linePoints(points);
+            this.bezierCurve(points);
             return ;
         }
         this.linePoints(points);
@@ -68,8 +70,8 @@ class Chart{
     }
     smoothCurve(points){
         let len = points.length, 
-            i = 0, 
-            controlPoint,
+            i = 1, 
+            controlPoint = {},
             controlPoint1,
             controlPoint2,
             direction,
@@ -85,36 +87,84 @@ class Chart{
 
                 假如有三个点A、B、C 控制点为B和C的中点，即cp1的坐标点为cp1x = (Bx + Cx) / 2,cp1y = (By + Cy) / 2;
             **/
-           for(; i < len - 1; i++){
-            //    controlPoint = {
-            //        x: (points[i - 1].x + points[i].x) / 2,
-            //        y: (points[i - 1].y + points[i].y) / 2 + 20
-            //    };
+        //    for(; i < len - 1; i++){
             //    由于二次贝塞尔曲线只有一个控制点，所以它永远只能画向一个方向弯曲的弧线，画不出s形曲线。要绘制s型曲线，需要使用三次贝塞尔曲线。
             //    this.ctx.quadraticCurveTo(controlPoint.x, controlPoint.y, points[i].x, points[i].y);
 
 
             //    三次贝塞尔曲线
-                direction = points[i + 1].y - points[i].y;
-                direction = direction >= 0 ? direction : direction * (-1)
-                controlPoint1 = {
-                    x: points[i].x + (points[i + 1].x - points[i].x) / 2 * t,
-                    y: points[i].y + direction / 2 * t, 
-                }
-                controlPoint2 = {
-                    x: points[i + 1].x - (points[i + 1].x - points[i].x) / 2 * t,
-                    y: points[i + 1].y + direction / 2 * t
-                }
+        //         direction = points[i + 1].y - points[i].y;
+        //         direction = direction >= 0 ? direction : direction * (-1)
+        //         controlPoint1 = {
+        //             x: points[i].x + (points[i + 1].x - points[i].x) / 2 * t,
+        //             y: points[i].y + direction / 2 * t, 
+        //         }
+        //         controlPoint2 = {
+        //             x: points[i + 1].x - (points[i + 1].x - points[i].x) / 2 * t,
+        //             y: points[i + 1].y + direction / 2 * t
+        //         }
 
-                // debugger
-                this.ctx.bezierCurveTo(controlPoint1.x, controlPoint1.y, controlPoint2.x, controlPoint2.y, points[i + 1].x, points[i + 1].y);
-           }
-           this.ctx.stroke();
-           this.ctx.closePath();
+        //         // debugger
+        //         this.ctx.bezierCurveTo(controlPoint1.x, controlPoint1.y, controlPoint2.x, controlPoint2.y, points[i + 1].x, points[i + 1].y);
+        //    }
+            for(;i < len - 2; i++){
+                controlPoint.x = (points[i].x + points[i + 1].x) / 2;
+                controlPoint.y = (points[i].y + points[i + 1].y) / 2;
+                this.ctx.quadraticCurveTo(points[i].x, points[i].y, controlPoint.x, controlPoint.y);
+            }
+            this.ctx.quadraticCurveTo(points[i].x, points[i].y, points[i + 1].x, points[i + 1].y);
+            this.ctx.stroke();
+            this.ctx.closePath();
         }else{
             this.linePoints(points);
         }
     }
+    bezierCurve(points){
+        const list = this.getCurveList(points);
+        this.ctx.lineWidth = 2;
+        this.ctx.beginPath();
+        for(let i = 0; i < list.length; i++){
+            this.ctx.moveTo(list[i].start.x, list[i].start.y);
+            this.ctx.bezierCurveTo(list[i].dot1.x, list[i].dot1.y, list[i].dot2.x, list[i].dot2.y, list[i].end.x, list[i].end.y);
+        }
+        this.ctx.strokeStyle = this.color(.5);
+        this.ctx.stroke();
+    }
+    getCurveList(points){
+        const t = 1/3;
+        const curveList = [];
+        let i = 1;
+        curveList.push({
+            start: points[0],
+            end: points[1],
+            dot1: points[0],
+            dot2: null
+        });
+        for(; i < points.length - 1; i++){
+            let cur, next, prev;
+            cur = points[i];
+            next = points[i + 1];
+            prev = points[i - 1];
+            let p1 = {
+                x: cur.x - t * (Math.abs(cur.x - prev.x)),
+                y: cur.y
+            };
+            let p2 = {
+                x: cur.x + t * (Math.abs(cur.x - next.x)),
+                y: cur.y
+            };
+            curveList[i - 1].dot2 = p1;
+            curveList.push({
+                start: cur,
+                end: next,
+                dot1: p2,
+                dot2: null
+            })
+        }
+        curveList[curveList.length - 1].dot2 = points[i];
+        return curveList;
+    }
+
     lineArea(points){
         let first, last, y;
         y = this.height;
