57b5d456c3437545f10331b35de65e5478f2c547
btea <2356281422@qq.com>
2019-08-01 17:04:01 +0800

    jsonp

---
+++ js/jsonp/jsonp.js
@@ -0,0 +1,17 @@
+const http = require('http');
+const fs = require('fs');
+
+http.createServer((request, response) => {
+    let url = request.url;
+    if(url === '/'){
+        fs.readFile('./index.html', (err, str) => {
+            response.end(str);
+        })
+    }
+    if(~url.indexOf('getData')){
+        let n, str;
+        n = url.split('?')[1].split('=')[1];
+        str = `${n}({data: [1,2,3,4,5,6]})`;
+        response.end(str);
+    }
+}).listen(2233);
