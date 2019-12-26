ec5990bd77f3a4093f3de4107d85af2903b2e2e6
btea <2356281422@qq.com>
2019-12-26 17:55:00 +0800

    socket

--- node/socket/index.js
+++ node/socket/index.js
@@ -14,9 +14,13 @@ let wsServer = new webSocketServer({port: 2333});
 wsServer.on('connection', function(socket){
     // socket.onmessage = function(event){
 
-    // }
+	// }
+	let i = 0
+	setInterval(() => {
+		socket.send('来自服务端消息：' + i++)
+	}, 5000)
     socket.on('message', function(message){
         console.log('接受到的消息:', message);
-        socket.send('来自服务端的消息:' + message);
+        // socket.send('来自服务端的消息:' + message);
     })
 })
