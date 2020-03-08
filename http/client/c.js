52db0ddcc7f7a6131fda81936ee5175c20dc6b2e
btea <2356281422@qq.com>
2020-03-08 16:48:56 +0800

    身份仁恒

--- http/client/c.js
+++ http/client/c.js
@@ -12,4 +12,28 @@ http.createServer((request, response) => {
             }
         })
     }
+    if (/^\/connection/.test(url)) {
+        let data = []
+        console.log(request.headers.cookie)
+        request.on('data', (chunk) => {
+            data += chunk
+        })
+        request.on('end', () => {
+            console.log(data)
+            // response.setHeader('Access-Control-Allow-Origin', 'http://192.168.1.3:2233');
+            response.setHeader('Connection', 'close');
+            response.setHeader('Content-Type', 'application/json;charset=utf-8');
+            response.setHeader('Set-Cookie', 'bir=4223;n="zys"');
+            response.statusCode = 200;
+            let obj = {
+                code: 200,
+                msg: 'ok',
+                data: {
+                    name: '灵凌林',
+                    sign: '说书人叹天下旧事如潮，听书人悲欢不过一壶新茶。'
+                }  
+            }
+            response.end(JSON.stringify(obj));
+        })
+    }
 }).listen(2233)
--- http/server/connection.js
+++ http/server/connection.js
@@ -5,15 +5,25 @@ http.createServer(function(request, response){
     response.setHeader('Access-Control-Allow-Origin', 'http://192.168.1.3:2233');
     if (/^\/connection/.test(url)) {
         let data = []
+        console.log(request.cookie)
         request.on('data', (chunk) => {
             data += chunk
         })
         request.on('end', () => {
             console.log(data)
             response.setHeader('Connection', 'close');
-            response.setHeader('Allow', 'GET,HEAD');
-            response.statusCode = 405;
-            response.end('connection');
+            response.setHeader('Content-Type', 'application/json;charset=utf-8');
+            response.setHeader('Set-Cookie', 'name="admin";id=123456;domain="http://192.168.1.3:2233"');
+            response.statusCode = 200;
+            let obj = {
+                code: 200,
+                msg: 'ok',
+                data: {
+                    name: '灵凌林',
+                    sign: '说书人叹天下旧事如潮，听书人悲欢不过一壶新茶。'
+                }  
+            }
+            response.end(JSON.stringify(obj));
         })
     }else {
         response.setHeader('Connection', 'close');
