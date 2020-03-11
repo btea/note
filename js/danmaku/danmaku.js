ae86d1e1464a20e4168ff1c1d51d5584c4acee52
btea <2356281422@qq.com>
2020-03-11 20:39:59 +0800

    danmaku

--- js/danmaku/danmaku.js
+++ js/danmaku/danmaku.js
@@ -1,1 +1,11 @@
 console.log('弹幕库')
+function request(url) {
+    let xhr = new XMLHttpRequest()
+    xhr.onreadystatechange = function() {
+        if(xhr.readyState === 4){
+            console.log(xhr)
+        }
+    }
+    xhr.open('get', url)
+    xhr.send('')
+}
