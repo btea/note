9af8d82c5522517dc8e385a98168847d6dbe9098
btea <2356281422@qq.com>
2020-04-20 15:47:12 +0800

    mouseevent

--- js/utils/load.js
+++ js/utils/load.js
@@ -10,12 +10,14 @@
             data = JSON.stringify(data, undefined, 4)
         }
         var blob = new Blob([data], {type: 'text/json'}),
-        e = document.createEvent('MouseEvents'),
+		// e = document.createEvent('MouseEvents'),
+		e = new MouseEvent('click');
         a = document.createElement('a');
-        a.download = filename
-        a.href = window.URL.createObjectURL(blob)
-        a.dataset.downloadurl = ['text/json', a.download, a.href].join(':');
-        e.initMouseEvent('click', true, false, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null)
+        a.download = filename;
+        a.href = window.URL.createObjectURL(blob);
+		a.dataset.downloadurl = ['text/json', a.download, a.href].join(':');
+		// initMouseEvent 方法已废弃， 用MouseEvent构造函数替代  https://developer.mozilla.org/zh-CN/docs/Web/API/MouseEvent/MouseEvent
+        // e.initMouseEvent('click', true, false, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null)
         a.dispatchEvent(e)
     }
 })(console)
