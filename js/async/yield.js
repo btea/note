80534cf37363aff4db5ed754dd3e60bef5035ef6
btea <2356281422@qq.com>
2020-08-13 21:50:44 +0800

    yield和异步函数对比

--- js/async/yield.js
+++ js/async/yield.js
@@ -30,18 +30,18 @@ const asyncReadFile = async function () {
 asyncReadFile();
 // async函数对Generator函数的改进，体现在在以下四点
 // https://es6.ruanyifeng.com/#docs/async
-(1) 内置执行器
-Generator函数执行必须依靠执行器，所有才有了co模块，而async函数自带执行器，也就是说，async函数的执行和普通函数一样，只要一行。
+// (1) 内置执行器
+// Generator函数执行必须依靠执行器，所有才有了co模块，而async函数自带执行器，也就是说，async函数的执行和普通函数一样，只要一行。
 
-(2) 更好的语义
-async 和 await ，比起yield 和 星号， 语义更清楚了。async表示函数里有异步操作，await表示紧跟在后面的表达式需要等待结果。
+// (2) 更好的语义
+// async 和 await ，比起yield 和 星号， 语义更清楚了。async表示函数里有异步操作，await表示紧跟在后面的表达式需要等待结果。
 
-(3) 更广的适用性
-co模块约定，yield命令后面只能是Thunk函数或者Promise对象，而async函数的await命令后面，可以是Promise对象和原始类型的值（数字、字符串、布尔值，但是会自动转成 resolved的Promise对象）。
+// (3) 更广的适用性
+// co模块约定，yield命令后面只能是Thunk函数或者Promise对象，而async函数的await命令后面，可以是Promise对象和原始类型的值（数字、字符串、布尔值，但是会自动转成 resolved的Promise对象）。
 
-(4) 返回值是Promise
-async函数的返回值是Promise对象，这比Generator函数的返回值是Iterator对象方便多了。你可以用then方法指定下一步操作。
-进一步说，async函数完全可以看作多个异步操作，包装成的一个Promise对象，而await命令就是内部then命令的语法糖。
+// (4) 返回值是Promise
+// async函数的返回值是Promise对象，这比Generator函数的返回值是Iterator对象方便多了。你可以用then方法指定下一步操作。
+// 进一步说，async函数完全可以看作多个异步操作，包装成的一个Promise对象，而await命令就是内部then命令的语法糖。
 
 
 
