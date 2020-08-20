e53dea3ef3def3125f5d7198a75f4820982e086f
btea <2356281422@qq.com>
2020-08-20 21:15:56 +0800

    canvas像素初始化

---
+++ canvas/canvas.js
@@ -0,0 +1,20 @@
+class CanvasInit{
+    init(canvas, width, height) {
+        let ctx = canvas.getContext('2d');
+        let backingStoreRatio = ctx.webkitBackingStorePixelRatio ||
+            ctx.mozBackingStorePixelRatio ||
+            ctx.msBackingStorePixelRatio ||
+            ctx.oBackingStorePixelRatio ||
+            ctx.backingStorePixelRatio || 1;
+        let devicePixelRatio = window.devicePixelRatio || 1;
+        let ratio = devicePixelRatio / backingStoreRatio;
+        canvas.style.width = `${width}px`
+        canvas.style.height = `${height}px`
+        ctx.scale(ratio, ratio)
+        canvas.width = width * ratio;
+        canvas.height = height * ratio;
+        return canvas
+    }
+}
+
+
